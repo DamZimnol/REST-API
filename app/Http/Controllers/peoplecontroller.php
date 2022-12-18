9748534427549707e9ae6228e7d086c0b151d323
DamZimnol <dz305426@student.polsl.pl>
2022-12-18 23:17:44 +0100

    Update peoplecontroller.php

--- app/Http/Controllers/peoplecontroller.php
+++ app/Http/Controllers/peoplecontroller.php
@@ -72,11 +72,11 @@ class peoplecontroller extends Controller
      */
     public function update(Request $request, people $people)
     {
-        $people = People::find($id);
+        $peoplefind = People::find($id);
 
-        $people->update(['name' => $name]);
+        $peoplefind->update(['name' => $name]);
 
-        return response($people, 200);
+        return response($peoplefind, 200);
     }
 
     /**
