5029860e891fdab8aeda726f251fb0bc40c49e70
DamZimnol <dz305426@student.polsl.pl>
2023-01-03 21:36:27 +0100

    corrected task - everything works

--- app/Http/Controllers/peoplecontroller.php
+++ app/Http/Controllers/peoplecontroller.php
@@ -4,88 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\people;
 use Illuminate\Http\Request;
+use App\Http\Resources\peopleresource;
 
 class peoplecontroller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $people=people::all();
         return response(\App\Http\Resources\peopleresource::collection($people),200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    public function create(Request $request)
+        {
+            $people = people::create($request->all());
+    
+            return response(new peopleresource($people), 201);
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $people = people::create($request->all());
-
-        return response(new PeopleResource($people), 201);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\people  $people
-     * @return \Illuminate\Http\Response
-     */
     public function show(people $people, int $id)
     {
         return response(people::find($id),200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\people  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(people $people)
+    public function update(Request $request, people $people, int $id)
     {
-        //
+        $ppl = people::find($id);
+
+        $ppl->update($request->all());
+
+        return response($ppl, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\people  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, people $people)
-    {
-        $peoplefind = people::find($id);
-
-        $peoplefind->update(['name' => $name]);
-
-        return response($peoplefind, 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\people  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(people $people)
+    public function destroy(people $people,int $id)
     {
         $people = people::destroy($id);
 
